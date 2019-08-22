43d7d16e6ae5ea8d80ed7838017d3eb3f811c9c0
Stamo-Gochev <stamo_gochev@yahoo.com>
2019-08-22 08:27:18 +0300

    chore: switch to concrete template parameter

---
+++ blazor-template-parameter/Models/Data/DataItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Models.Data
+{
+
+    public class DataItem
+    {
+        public int PetId { get; set; }
+        public string Name { get; set; }
+    }
+}
